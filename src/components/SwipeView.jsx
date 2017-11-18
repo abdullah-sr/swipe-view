880eb34b7ef03cf5861166f9b548745ec8eecbe9
Abdullah <hrfssa@gmail.com>
2017-11-18 14:03:12 -0800

    added material ui

--- src/components/SwipeView.jsx
+++ src/components/SwipeView.jsx
@@ -19,14 +19,14 @@ class SwipeView extends Component {
         fetch('https://renthoop-production.appspot.com/_ah/api/renthoopendpoint/v1/potentialroommate/181004659047340?latitude=34.072926&longitude=-118.442986&state=CA&locality=West%20Hollywood&radius=32000.000000').then((response) => {
             return response.json();
         }).then((json) => {
-            //console.log(json);
-            //for (let item of json.items) {
+            console.log(json);
+            for (let item of json.items) {
                 self.setState(
                     (state) => {
-                        //return {images: state.images.concat(`https://graph.facebook.com/${item.potentialRoommate.userID}/picture?width=400&height=400`)}
+                        return {images: state.images.concat(`https://graph.facebook.com/${item.potentialRoommate.userID}/picture?width=400&height=400`)}
                     }
                 )
-            //}
+            }
         }).catch((ex) => {
             console.log('parsing failed', ex);
         });
