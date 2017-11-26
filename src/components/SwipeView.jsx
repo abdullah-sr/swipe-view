558e7699ed7c40ef1da08c09da52156546533508
Abdullah <hrfssa@gmail.com>
2017-11-26 12:56:27 -0800

    cleaned up

--- src/components/SwipeView.jsx
+++ src/components/SwipeView.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-import SwipeableViews from './SwipeableViews';
-import { virtualize } from 'react-swipeable-views-utils';
+import SwipeableViews from 'react-swipeable-views';
+// import { virtualize } from 'react-swipeable-views-utils';
 import { CircularProgress } from 'material-ui/Progress';
 import { blue } from 'material-ui/colors';
 import Card, { CardImage } from './Card';
+
+
+// const VirtualizeSwipeableViews = virtualize(SwipeableViews);
 
 
 class SwipeView extends Component {
@@ -22,34 +25,34 @@ class SwipeView extends Component {
         };
 
         this.listItems = this.listItems.bind(this);
-        this.slideRenderer = this.slideRenderer.bind(this);
+        // this.slideRenderer = this.slideRenderer.bind(this);
 
-        let self = this;
-        fetch('https://renthoop-production.appspot.com/_ah/api/renthoopendpoint/v1/potentialroommate/181004659047340?latitude=34.072926&longitude=-118.442986&state=CA&locality=West%20Hollywood&radius=32000.000000').then((response) => {
-            return response.json();
-        }).then((json) => {
-            console.log(json);
-            for (let item of json.items) {
-                self.setState(
-                    (state) => {
-                        return { images: state.images.concat(`https://graph.facebook.com/${item.potentialRoommate.userID}/picture?width=400&height=400`) };
-                    }
-                )
-            }
-            this.setState({ loading: false });
-        }).catch((ex) => {
-            console.log('parsing failed', ex);
-        });
+        this.fetchData();
     }
 
 
-    slideRenderer({ index }) {
-        console.log(index);
-        return (
-            <Card key={index}>
-                <CardImage src={this.state.images[index]} />
-            </Card>
-        );
+    // slideRenderer({ key, index }) {
+    //     //console.log(key, index);
+    //     return (
+    //
+    //         <Slide key={key} index={index}/>
+    //     );
+    // }
+
+    async fetchData() {
+        try {
+            const response = await fetch('https://renthoop-production.appspot.com/_ah/api/renthoopendpoint/v1/potentialroommate/181004659047340?latitude=34.072926&longitude=-118.442986&state=CA&locality=West%20Hollywood&radius=32000.000000');
+            const reponseJson = await response.json();
+            console.log(reponseJson);
+            for (const item of reponseJson.items) {
+                this.setState((state) => {
+                    return { images: state.images.concat(`https://graph.facebook.com/${item.potentialRoommate.userID}/picture?width=400&height=400`) };
+                });
+            }
+            this.setState({ loading: false });
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     listItems() {
@@ -64,18 +67,13 @@ class SwipeView extends Component {
 
     render() {
         if (this.state.loading) {
-            return (<CircularProgress size={50} style={{ color: blue[500] }}/>);
+            return (<CircularProgress size={50} style={{ color: blue[500] }} />);
         }
-        const VirtualizeSwipeableViews = virtualize(SwipeableViews);
         return (
-            <VirtualizeSwipeableViews slideRenderer={this.slideRenderer} overscanSlideAfter={7} overscanSlideBefore={7}
-                                      slideCount={this.state.images.length} slideStyle={this.styles.slideStyle}/>
+            <SwipeableViews slideStyle={this.styles.slideStyle}>
+                {this.listItems()}
+            </SwipeableViews>
         );
-        // return (
-        //     <SwipeableViews slideStyle={this.styles.slideStyle}>
-        //         {this.listItems()}
-        //     </SwipeableViews>
-        // );
     }
 }
 
